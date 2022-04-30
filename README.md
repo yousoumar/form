594f752d6c4639c504c8de7f06a34916ddf9f9ae
yousoumar <oumaiyr@gmail.com>
2022-04-30 09:03:27 +0200

    optimizes ui and updates readme

--- README.md
+++
@@ -1,8 +0,0 @@
-Il s'agit d'un formulaire avec validation développé en `HTML`, `CSS`, et `JavaScript`.
-
-Tester en cliquant sur l'image ci-dessous ou sur le lien en bas de la section `About`.
-
-<a href = "https://yousoumar.github.io/form/"><img src = "images/screenshot.png"></img></a>
-
-  
-
--- js/app.js
+++ js/app.js
@@ -1,75 +1,99 @@
 "use strict";
 
+const inputs = document.querySelectorAll('input');
 const userInput = document.querySelector('input#user');
 const emailInput = document.querySelector('input#email');
 const passwordInput = document.querySelector('input#password');
 const secondPasswordInput= document.querySelector('input#second-password');
-const validationIcons = document.querySelectorAll('.validation-icon');
-const alerts = document.querySelectorAll('.alert');
 const mainAlert = document.querySelector('.main-alert');
 const lines = document.querySelectorAll('.line div');
 const sumbitMessage = document.querySelector('.submit-message');
 const myForm = document.querySelector('form');
 
+// object used below to check if all the inputs are ok
+// we have string properties because of the lat one 
+
 const validation = {
-    userName : 0,
-    email : 0,
-    password : 0
+    'user' : 0,
+    'email' : 0,
+    'password' : 0,
+    'second-password' : 0
 }
-userInput.addEventListener('input', (e) => {
+
+inputs.forEach(input => {
+    input.addEventListener('focus', (e)=>{
+        resetField(e.currentTarget);
+    });
+});
+
+// username input
+
+userInput.addEventListener('blur', (e) => {
+    mainAlert.style.display = "none"; 
     const regexUser = /[^a-z]/g
-    const regexUser2 = /\s/g;
-    validationIcons[0].style.display = "block";
-    if(e.target.value.search(regexUser) === -1 && e.target.value.search(regexUser2) === -1 && e.target.value.trim().length >= 3) {
-        validationIcons[0].src = "images/check.svg";
-        alerts[0].style.display = "none";
-        validation.userName = 1;
+
+    if(e.target.value.search(regexUser) === -1  && e.target.value.length >= 3) {
+        validation['user'] = 1;
+        validateField(e.currentTarget);
     }   
     else {
+        validation['user'] = 0;
+        inValidateField(e.currentTarget);
         
-        validationIcons[0].src = "images/error.svg";
-        alerts[0].style.display = "block";
-        validation.userName = 0;
+       
     }
 
 });
 
-emailInput.addEventListener('input', (e) => {
-
+// email input
+emailInput.addEventListener('blur', (e) => {
+    mainAlert.style.display = "none"; 
     const regexEmail = /\S+@\S+\.\S+/;
-    
     if(e.target.value.search(regexEmail) === 0){
+        validation['email'] = 1;
+        validateField(e.currentTarget);
 
-        validationIcons[1].style.display = "block";
-        validationIcons[1].src = "images/check.svg";
-        alerts[1].style.display = "none";
-        validation.email = 1;
+    } else {
 
-    } else if(e.target.value.search(regexEmail) === -1) {
-
-        validationIcons[1].style.display = "block";
-        validationIcons[1].src = "images/error.svg";
-        alerts[1].style.display = "block";
-        validation.email = 0;
+        validation['email'] = 0;
+        inValidateField(e.currentTarget);
 
     }
 
 });
 
+
+// password input
 const passwordValidation = {
-    symbol : 0,
-    lowercase : 0,
-    uppercase:0,
-    number : 0
-}
-const specialChar = /[^a-zA-Z0-9]/;
-const lowercase = /[a-z]/;
-const uppercase = /[A-Z]/;
-const number = /[0-9]/;
+        symbol : 0,
+        lowercase : 0,
+        uppercase:0,
+        number : 0,
+        space:0
+    }
 
 passwordInput.addEventListener('input', (e) => {
+    secondPasswordInput.value ="";
+    validation['second-password'] = 0;
+    resetField(secondPasswordInput);
 
+    lines[0].style.display = 'none';
+    lines[1].style.display = 'none';
+    lines[2].style.display = 'none';
+    
+});
 
+passwordInput.addEventListener('blur', (e) => {
+    mainAlert.style.display = "none"; 
+      
+    const specialChar = /[^a-zA-Z0-9]/;
+    const lowercase = /[a-z]/;
+    const uppercase = /[A-Z]/;
+    const number = /[0-9]/;
+    const space =/\s/g;
+
+    
+   
     if(e.target.value.search(specialChar) !== -1){
         passwordValidation.symbol = 1;
     }else{
@@ -91,7 +115,11 @@ passwordInput.addEventListener('input', (e) => {
     }else{
         passwordValidation.number = 0;
     }
-
+    if(e.target.value.search(space) !== -1){
+        passwordValidation.space = 0;
+    }else{
+        passwordValidation.space = 1;
+    }
     let testValue = true;
     for(const property in passwordValidation){
         if(passwordValidation[property] == 0){
@@ -99,23 +127,17 @@ passwordInput.addEventListener('input', (e) => {
         }
     }
     
-    validationIcons[2].style.display = "block";
     if(!testValue){
-        alerts[2].style.display = "block";
-        validationIcons[2].src = "images/error.svg";
+        validation['password'] = 0;
+        inValidateField(e.currentTarget);
+        
         lines[0].style.display = 'none';
         lines[1].style.display = 'none';
         lines[2].style.display = 'none';
-        secondPasswordInput.value ="";
-        validation.password =0;
-        validationIcons[3].style.display = "none";
-        alerts[3].style.display = "none";
-
-
       
     } else {
-        alerts[2].style.display = "none";
-        validationIcons[2].src = "images/check.svg";
+        validation['password'] = 1;
+        validateField(e.currentTarget);
         if(e.target.value.length <= 6 && e.target.value.length > 0){
             lines[0].style.display = 'block';
             lines[1].style.display = 'none';
@@ -143,11 +165,14 @@ passwordInput.addEventListener('input', (e) => {
 
 });
 
+// second password input
+// blow is so we can not put in if the first password input is not yet ok
 secondPasswordInput.addEventListener('input', (e) => {
-  
+
     let firstPasswordTest = true;
     for (const prop in passwordValidation) {
         if (passwordValidation[`${prop}`] == 0) {
+            
             firstPasswordTest = false;
         }
     }
@@ -155,24 +180,29 @@ secondPasswordInput.addEventListener('input', (e) => {
         e.target.value="";
         return;
     }
-    if(e.target.value.length === 0){
-        validationIcons[3].style.display = "block";
-        validationIcons[3].src = "images/error.svg";
-        validation.password = 0;
+  
+});
+secondPasswordInput.addEventListener('blur', (e)=>{
+    let firstPasswordTest = true;
+    for (const prop in passwordValidation) {
+        if (passwordValidation[`${prop}`] == 0) {
+            
+            firstPasswordTest = false;
+        }
     }
-    else if(e.target.value === passwordInput.value){
-        validationIcons[3].style.display = "block";
-        validationIcons[3].src = "images/check.svg";
-        alerts[3].style.display = "none";
-        validation.password = 1;
-    } else {
-        validationIcons[3].style.display = "block";
-        validationIcons[3].src = "images/error.svg";
-        alerts[3].style.display = "block";
-        validation.password = 0;
+    if (firstPasswordTest){
+      
+        if(e.target.value === passwordInput.value){
+            validateField(e.currentTarget);
+            validation['second-password'] = 1;
+        } else {
+            inValidateField(e.currentTarget);
+            validation['second-password'] = 0;
+        }
     }
 
 });
+
 
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -180,12 +210,39 @@ myForm.addEventListener('submit', (e) => {
     for (const prop in validation) {
         if (validation[`${prop}`] == 0 ) {
             testValidation = false;
+            document.querySelector(`input#${prop}`).style.borderColor = "crimson";
         }
     }
     if (!testValidation){
         mainAlert.style.display = "block";    
         return;
     }
-    sumbitMessage.style.display = "block";
     myForm.style.display = "none";
+    window.scrollTo(0, 0);
+    sumbitMessage.style.display = "block";
+    
 });
+
+function resetField(field){
+    mainAlert.style.display = "none"; 
+    document.querySelector(`img.${field.getAttribute('id')}`).style.display = "none";
+    document.querySelector(`div.${field.getAttribute('id')}`).style.display = "none";
+    field.style.borderColor = "rgba(0,0,0,0.3)";
+}
+function validateField(field){
+    
+    document.querySelector(`img.${field.getAttribute('id')}`).style.display ="block";
+    document.querySelector(`img.${field.getAttribute('id')}`).src = "images/check.svg";
+    document.querySelector(`div.${field.getAttribute('id')}`).style.display = "none";
+    field.style.borderColor = "green";
+}
+function inValidateField(field){
+    
+    if (field.value !== ""){
+        document.querySelector(`img.${field.getAttribute('id')}`).style.display ="block";
+        document.querySelector(`img.${field.getAttribute('id')}`).src = "images/error.svg";
+        document.querySelector(`div.${field.getAttribute('id')}`).style.display = "block";
+        field.style.borderColor = "crimson";
+    }
+    
+}
